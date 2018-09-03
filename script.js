f491c2fd0f6c32d3f31b898473d60fa3cd10bb0e
Nehat Dedinca <nehat.dedinca@prosperoware.com>
2018-09-03 22:37:32 +0200

    Handle main functions to display data

--- script.js
+++ script.js
@@ -10,32 +10,71 @@ var ponyName = "Fluttershy"; // ugh
 
 // difficulty between 0 and 10
 var difficulty = 1;
+
+// maze id and state
 var mazeId = "";
- var mazeParams = {
-    "maze-width": 15,
+var mazeState = {};
+
+// maze visuals (ascii art)
+var mazeViz = "";
+var mazeParams = {
+   "maze-width": 15,
     "maze-height": 15,
     "maze-player-name": "Fluttershy",
-    "difficulty": 1
+   "difficulty": 1
 };
 
 // setup standard ajax params
 $.ajaxSetup({
-  dataType: "json",
-  contentType: "application/json; charset=utf-8"
+ dataType: "json",
+ contentType: "application/json; charset=utf-8"
 })
- $(document).on('click', '.start', function(){
-  // get maze_id from API when user starts game
-  $.ajax({
-    url: apiURL,
-    data: JSON.stringify(mazeParams),
-    method: "POST"
-  }).done(function(data){
-    console.log("== SUCCESS ==");
-    console.log(data);
+
+//////////////////////////////
+//        FUNCTIONS         //
+//////////////////////////////
+function getMazeState(id){
+ $.ajax({
+   dataType: "json",
+   contentType: "application/json; charset=utf-8",
+   url: apiURL+"/"+id,
+   method: "GET"
+ }).done(function(data){
+   mazeState = data;
+   console.log(mazeState);
+   getMazeVisual(id)
+ });
+}
+
+function getMazeVisual(id){
+ $.ajax({
+   dataType: "text",
+   url: apiURL+"/"+mazeId+"/print",
+   method: "GET"
+ }).done(printMazeState);
+}
+
+function printMazeState(mazeViz){
+ $('.maze').empty().append(mazeViz);
+}
+
+$(document).on('click', '.start', function(){
+ // get maze_id from API when user starts game
+ $.ajax({
+   dataType: "json",
+   contentType: "application/json; charset=utf-8",
+   url: apiURL,
+   data: JSON.stringify(mazeParams),
+   method: "POST"
+ }).done(function(data){
+   console.log("== SUCCESS ==");
+   console.log(data);
     mazeId = data.maze_id;
-  }).fail(function(err){
-    console.log("== ERROR ==");
-    console.log(err);
+   getMazeState(mazeId);
+    $(".gamesetup").fadeOut(100);
+ }).fail(function(err){
+   console.log("== ERROR ==");
+   console.log(err);
   });
 });
 
@@ -44,7 +83,8 @@ $(document).on('change', '.setupfield[type="number"]', function(){
   let number = $(this).val();
   let max = parseInt($(this).attr('max'));
   let min = parseInt($(this).attr('min'));
-   if(number > max){
+
+  if(number > max){
     $(this).val(max);
   }else if(number < min){
     $(this).val(min);
