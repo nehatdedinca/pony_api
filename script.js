1352969e79b6a2bae7cbc5c72458c8f199c4af77
Nehat Dedinca <nehat.dedinca@prosperoware.com>
2018-09-03 23:09:13 +0200

    Restructuring the game's logic and CSS styles

--- script.js
+++ script.js
@@ -1,101 +1,134 @@
+//////////////////////////////////
+//         SETUP & VARS         //
+//////////////////////////////////
+ $.ajaxSetup({
+  contentType: "application/json; charset=utf-8"
+})
+
 // API URL
 var apiURL = "https://ponychallenge.trustpilot.com/pony-challenge/maze"
 
-// width and height of maze between 15 and 25
-var mazeWidth = 15;
-var mazeHeight = 15;
-// ponyname must be a valid name of a pony, why
-var ponyName = "Fluttershy"; // ugh
+// width and height of maze, between 15 and 25
 // difficulty between 0 and 10
-var difficulty = 1;
+// ponyname must be a valid name of a pony, why
+var mazeWidth, mazeHeight, difficulty, pony;
 // maze id and state
 var mazeId = "";
 var mazeState = {};
-// maze visuals (ascii art)
+// maze visuals (ascii art!!)
 var mazeViz = "";
-
-var mazeParams = {
-    "maze-width": 15,
-    "maze-height": 15,
-    "maze-player-name": "Fluttershy",
-    "difficulty": 1
-};
+// current game state
+var gameState = "";
+// empty object for maze parameters
+var mazeParams = {};
 
 //////////////////////////////
 //        FUNCTIONS         //
 //////////////////////////////
-function getMazeId(){
+// == AJAX Functions == //
+ function getMazeId(params){
+  // receives a maze-id from api, used for all other calls
  $.ajax({
    dataType: "json",
-   contentType: "application/json; charset=utf-8",
    url: apiURL,
-   data: JSON.stringify(mazeParams),
+   data: JSON.stringify(params),
    method: "POST"
  }).done(function(data){
-   console.log("== SUCCESS ==");
-   console.log(data);
     mazeId = data.maze_id;
    getMazeState(mazeId);
     $(".gamesetup").fadeOut(100);
- }).fail(function(err){
-   console.log("== ERROR ==");
-   console.log(err);
- });
+  }).fail(ajaxFail);
 }
 function getMazeState(id){
+  // retrieves state from server, then renders visuals
+  // NB - state is currently not utilized for any purpose
+
  gameStarted = true;
  $.ajax({
    dataType: "json",
-   contentType: "application/json; charset=utf-8",
     url: apiURL+"/"+id,
     method: "GET"
   }).done(function(data){
     mazeState = data;
-    console.log(mazeState);
-    getMazeVisual(id)
-  });
+    getMazeVisual(id);
+    }).fail(ajaxFail);
 }
 
 function getMazeVisual(id){
   $.ajax({
     dataType: "text",
-    url: apiURL+"/"+mazeId+"/print",
+    mimeType: "text/plain; charset=utf-8",
+    url: apiURL+"/"+id+"/print",
     method: "GET"
-  }).done(printMazeState);
+  }).done(printMazeState).fail(ajaxFail);
+  }
+   function postMazeMove(id, dir){
+    // posts direction to maze, updates visuals on success
+    $.ajax({
+      dataType: "json",
+      url: apiURL+"/"+id,
+      method: "POST",
+      data: JSON.stringify({direction: dir})
+    }).done(function(data){
+      console.log(data);
+      gameState = data.state;
+      getMazeState(id);
+      printMsg(data["state-result"])
+    }).fail(ajaxFail);
+  }
+   // == Non-AJAX Functions == //
+   function printMsg(msg){
+    $('#messages').append($('<li>').text(msg));
+    scrollBottom();
 }
 
 function printMazeState(mazeViz){
   $('.maze').empty().append(mazeViz);
 }
-$(document).on('click', '.start', function(){
- // get maze_id from API when user starts game
- $.ajax({
-   dataType: "json",
-   contentType: "application/json; charset=utf-8",
-   url: apiURL,
-   data: JSON.stringify(mazeParams),
-   method: "POST"
- }).done(function(data){
-   console.log("== SUCCESS ==");
-   console.log(data);
-    mazeId = data.maze_id;
-   getMazeState(mazeId);
-    $(".gamesetup").fadeOut(100);
- }).fail(function(err){
-   console.log("== ERROR ==");
-   console.log(err);
- });
- getMazeId();
-});
-$(document).on('change', '.setupfield[type="number"]', function(){
-  //reset value to min or max if value exceeds either
-  let number = $(this).val();
-  let max = parseInt($(this).attr('max'));
-  let min = parseInt($(this).attr('min'));
 
-  if(number > max){
-    $(this).val(max);
-  }else if(number < min){
-    $(this).val(min);
+function ajaxFail(err){
+  console.log("== ERROR ==");
+  console.log(err);
+}
+ function scrollBottom(){
+  let scrollbox = $('.msg_box');
+  let e = $('#messages');
+  scrollbox.scrollTop(e.height());
+}
+ ///////////////////////////////////
+//         EVENT HANDLING        //
+///////////////////////////////////
+
+$(document).on('click', '.start', function(){
+  // collect game setup and get maze_id from api when user starts
+  mazeWidth = $('#maze-width').val();
+  mazeHeight = $('#maze-height').val();
+  difficulty = $('#maze-difficulty').val();
+  pony = $('#maze-player-name').val();
+   mazeParams = {
+    "maze-width": parseInt(mazeWidth, 10),
+    "maze-height": parseInt(mazeHeight, 10),
+    "maze-player-name": pony,
+    "difficulty": parseInt(difficulty, 10)
   }
+  getMazeId(mazeParams);
 });
+ $(document).on('click', '.controls', function(){
+  // posts chosen direction
+  let direction = $(this).attr('data-dir');
+  printMsg("You attempt to move "+direction+".");
+  postMazeMove(mazeId, direction);
+});
+
+$(document).on('change', '.setupfield[type="number"]', function(){
+   //reset value to min or max if value exceeds either
+   let number = $(this).val();
+   let max = parseInt($(this).attr('max'));
+   let min = parseInt($(this).attr('min'));
+
+   if(number > max){
+     $(this).val(max);
+   }else if(number < min){
+     $(this).val(min);
+   }
+ });
