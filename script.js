b3c328a5d3389c868cb3584da1f766dd433ec789
Nehat Dedinca <nehat.dedinca@prosperoware.com>
2018-09-04 19:44:28 +0200

    Handle position and walls

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //////////////////////////////////
 //         SETUP & VARS         //
 //////////////////////////////////
- $.ajaxSetup({
+
+$.ajaxSetup({
   contentType: "application/json; charset=utf-8"
 })
 
@@ -21,37 +22,54 @@ var mazeViz = "";
 var gameState = "";
 // empty object for maze parameters
 var mazeParams = {};
+// "ai" controlled pony
+var AUTO = false;
 
 //////////////////////////////
 //        FUNCTIONS         //
 //////////////////////////////
+
 // == AJAX Functions == //
- function getMazeId(params){
+
+function getMazeId(params){
   // receives a maze-id from api, used for all other calls
- $.ajax({
-   dataType: "json",
-   url: apiURL,
-   data: JSON.stringify(params),
-   method: "POST"
- }).done(function(data){
+  $.ajax({
+    dataType: "json",
+    url: apiURL,
+    data: JSON.stringify(params),
+    method: "POST"
+  }).done(function(data){
     mazeId = data.maze_id;
-   getMazeState(mazeId);
+    getMazeState(mazeId);
+
     $(".gamesetup").fadeOut(100);
   }).fail(ajaxFail);
 }
+
+
 function getMazeState(id){
   // retrieves state from server, then renders visuals
   // NB - state is currently not utilized for any purpose
-
- gameStarted = true;
- $.ajax({
-   dataType: "json",
+  gameStarted = true;
+  $.ajax({
+    dataType: "json",
     url: apiURL+"/"+id,
     method: "GET"
   }).done(function(data){
     mazeState = data;
-    getMazeVisual(id);
-    }).fail(ajaxFail);
+    gameState = data['game-state'].state.toLowerCase();
+
+    console.log(gameState);
+    getMazeVisual(id)
+
+    if(AUTO && gameState == "active"){
+      let walls = buildWallsAtPony(data);
+      let dirs = getValidDirections(walls);
+      let randDir = getRandomInt(dirs.length);
+
+      postMazeMove(mazeId, dirs[randDir]);
+    }
+  }).fail(ajaxFail);
 }
 
 function getMazeVisual(id){
@@ -61,25 +79,29 @@ function getMazeVisual(id){
     url: apiURL+"/"+id+"/print",
     method: "GET"
   }).done(printMazeState).fail(ajaxFail);
-  }
-   function postMazeMove(id, dir){
-    // posts direction to maze, updates visuals on success
-    $.ajax({
-      dataType: "json",
-      url: apiURL+"/"+id,
-      method: "POST",
-      data: JSON.stringify({direction: dir})
-    }).done(function(data){
-      console.log(data);
-      gameState = data.state;
-      getMazeState(id);
-      printMsg(data["state-result"])
-    }).fail(ajaxFail);
-  }
-   // == Non-AJAX Functions == //
-   function printMsg(msg){
-    $('#messages').append($('<li>').text(msg));
-    scrollBottom();
+}
+
+
+function postMazeMove(id, dir){
+  // posts direction to maze, updates visuals on success
+  $.ajax({
+    dataType: "json",
+    url: apiURL+"/"+id,
+    method: "POST",
+    data: JSON.stringify({direction: dir})
+  }).done(function(data){
+    console.log(data);
+    gameState = data.state;
+    getMazeState(id);
+    printMsg(data["state-result"])
+  }).fail(ajaxFail);
+}
+
+// == Non-AJAX Functions == //
+
+function printMsg(msg){
+  $('#messages').append($('<li>').text(msg));
+  scrollBottom();
 }
 
 function printMazeState(mazeViz){
@@ -94,29 +116,83 @@ function ajaxFail(err){
     $("#maze-player-name").val('').attr('placeholder', err.responseText);
   }
 }
- function scrollBottom(){
+
+function scrollBottom(){
   let scrollbox = $('.msg_box');
   let e = $('#messages');
   scrollbox.scrollTop(e.height());
 }
- ///////////////////////////////////
+
+function buildWallsAtPony(mazestate){
+  // each position can have walls at "west" and "north"
+  // find walls by looking at nth index in maze data array
+  let ponyPos = mazestate.pony[0];
+
+  let wallsPony = mazestate.data[ponyPos];
+  let wallsRight = mazestate.data[ponyPos+1];
+  let wallsBelow = ponyPos+mazeWidth < (mazeWidth*mazeHeight) ? mazestate.data[ponyPos+mazeWidth] : ["north"];
+
+  let walls = {
+    pony: wallsPony,
+    right: wallsRight,
+    below: wallsBelow
+  };
+
+  return walls;
+}
+
+function getValidDirections(walls){
+  let dirs = ["north", "west", "east", "south"];
+  for(x in walls){
+    for(var i = 0; i < walls[x].length; i++){
+      let index;
+      // remove directions depending on key name
+      // pretty clunky implementation
+      if(x === 'pony'){
+        index = dirs.indexOf(walls[x][i]);
+      }
+      if(x === 'below' && walls[x][i] === "north"){
+        index = dirs.indexOf("south");
+      }
+      if(x === 'right' && walls[x][i] === "west"){
+        index = dirs.indexOf("east");
+      }
+      if(index >= 0){
+        dirs.splice(index, 1);
+      }
+    }
+  }
+  return dirs;
+}
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+///////////////////////////////////
 //         EVENT HANDLING        //
 ///////////////////////////////////
 
+
+
 $(document).on('mousedown', '.start', function(){
   // collect game setup and get maze_id from api when user starts
-  mazeWidth = $('#maze-width').val();
-  mazeHeight = $('#maze-height').val();
-  difficulty = $('#maze-difficulty').val();
+  mazeWidth = parseInt($('#maze-width').val(), 10);
+  mazeHeight = parseInt($('#maze-height').val(), 10);
+  difficulty = parseInt($('#maze-difficulty').val(), 10);
   pony = $('#maze-player-name').val();
-   mazeParams = {
-    "maze-width": parseInt(mazeWidth, 10),
-    "maze-height": parseInt(mazeHeight, 10),
+
+  mazeParams = {
+    "maze-width": mazeWidth,
+    "maze-height": mazeHeight,
     "maze-player-name": pony,
-    "difficulty": parseInt(difficulty, 10)
+    "difficulty": difficulty
   }
   getMazeId(mazeParams);
 });
+
+
+
 $(document).on('mousedown', '.controls', function(){
   // posts chosen direction
   let direction = $(this).attr('data-dir');
@@ -127,14 +203,14 @@ $(document).on('mousedown', '.controls', function(){
 });
 
 $(document).on('change', '.setupfield[type="number"]', function(){
-   //reset value to min or max if value exceeds either
-   let number = $(this).val();
-   let max = parseInt($(this).attr('max'));
-   let min = parseInt($(this).attr('min'));
+  //reset value to min or max if value exceeds either
+  let number = $(this).val();
+  let max = parseInt($(this).attr('max'));
+  let min = parseInt($(this).attr('min'));
 
-   if(number > max){
-     $(this).val(max);
-   }else if(number < min){
-     $(this).val(min);
-   }
- });
+  if(number > max){
+    $(this).val(max);
+  }else if(number < min){
+    $(this).val(min);
+  }
+});
